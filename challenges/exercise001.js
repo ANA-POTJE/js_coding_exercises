ec757727fbf0fb6d53b8ac8dfdda8b717633e476
anapotje <anapotje@gmail.com>
2020-08-27 19:41:23 +0100

    Implemented first exercise - getMiddleCharacter

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -23,7 +23,21 @@ function getSalePrice(originalPrice, reduction) {
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+
+  function extractMiddle(str) {
+    var position;
+    var length;
+
+    if(str.length % 2 == 1) {
+        position = str.length / 2;
+        length = 1;
+    } else {
+        position = str.length / 2 - 1;
+        length = 2;
+    }
+    return str.substring(position, position + length)
+  }
+  return(extractMiddle(str));
 }
 
 function reverseWord(word) {
