e64a38cf798e259447f542ebc97577ec1e711e9e
anapotje <anapotje@gmail.com>
2020-08-29 17:27:11 +0100

    Implemented second exercise - COMPLETED

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -24,7 +24,12 @@ function countSheep(arr) {
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
+  if (((person.address.postCode.substr(0, 1)) == "M") && (person.address.city == 'Manchester')){
+    return(true)
+  } else{
+    return(false)
+  }
 }
 
 module.exports = {
